1a708bd5bdb22b94e3ce82ec35f50a49bf6ec20a
jeevanabhi-r <jeevanabhi.r@gmail.com>
2026-04-21 19:13:58 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,18 @@
-// Import Firebase (MODULAR)
+// Firebase v10 (MODULAR)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your config
 const firebaseConfig = {
- apiKey: "AIzaSyBozFPbqUn7nTwbjRC027ACfbAakY8ocwE",
+  apiKey: "AIzaSyBozFBbqUn7nTwbjRC027ACfBaAkY8ocWE",
   authDomain: "clubhub2-226c0.firebaseapp.com",
   projectId: "clubhub2-226c0",
-  storageBucket: "clubhub2-226c0.appspot.com"
+  storageBucket: "clubhub2-226c0.appspot.com", // ✅ FIXED
   messagingSenderId: "473457215975",
   appId: "1:473457215975:web:17c1eb7278a428be175542"
 };
 
-// Initialize
 const app = initializeApp(firebaseConfig);
 
-// Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
