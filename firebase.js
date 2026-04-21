4d9ac25f1c9013d41893b3375f549852d57cb478
jeevanabhi-r <jeevanabhi.r@gmail.com>
2026-04-21 19:33:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBozFBbqUn7nTwbjRC027ACfBaAkY8ocWE",
   authDomain: "clubhub2-226c0.firebaseapp.com",
   projectId: "clubhub2-226c0",
-  storageBucket: "clubhub2-226c0.appspot.com",
+  storageBucket: "clubhub2-226c0.appspot.com", // ✅ FIXED
   messagingSenderId: "473457215975",
   appId: "1:473457215975:web:17c1eb7278a428be175542"
 };
