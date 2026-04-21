56617b678b48f0caf949d03b555eef45f07e20b8
jeevanabhi-r <jeevanabhi.r@gmail.com>
2026-04-21 19:09:31 +0530

    Add Firebase initialization and configuration

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import Firebase (MODULAR)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Your config
+const firebaseConfig = {
+ apiKey: "AIzaSyBozFPbqUn7nTwbjRC027ACfbAakY8ocwE",
+  authDomain: "clubhub2-226c0.firebaseapp.com",
+  projectId: "clubhub2-226c0",
+  storageBucket: "clubhub2-226c0.appspot.com"
+  messagingSenderId: "473457215975",
+  appId: "1:473457215975:web:17c1eb7278a428be175542"
+};
+
+// Initialize
+const app = initializeApp(firebaseConfig);
+
+// Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
